04606d7a96831026fde377d0fab1a7c661c73bac
Anil Ata <anilatacevik@gmail.com>
2026-04-05 22:33:57 +0300

    feat: DangerZoneController and FlightExamManager updated for missile launch, homing and hit events

--- Assets/Scripts/DangerZoneController.cs
+++ Assets/Scripts/DangerZoneController.cs
@@ -5,33 +5,48 @@ using System.Collections;
 public class DangerZoneController : MonoBehaviour 
 {
     [SerializeField] private FlightExamManager examManager; 
-    //[SerializeField] private MissileLauncher missileLauncher; 
+    [SerializeField] private MissileLauncher missileLauncher; 
     [SerializeField] private float missileDelay = 5f; 
  
-    private Coroutine activeCountdown; 
+    private Coroutine activeCountdown;
+    private Transform playerTransform;
  
     private void OnTriggerEnter(Collider collision) 
     { 
-        if (collision.CompareTag("Player"))
-        {
-            examManager.EnterDangerZone();
+        if (!collision.CompareTag("Player")) return;
 
+        playerTransform = collision.transform;
+
+        examManager.EnterDangerZone();
+
+        if (activeCountdown == null)
             activeCountdown = StartCoroutine(MissileCountdown());
-        }
     } 
  
     private void OnTriggerExit(Collider collision) 
     { 
-        if (collision.CompareTag("Player"))
+        if (!collision.CompareTag("Player")) return;
+
+        if (activeCountdown != null)
         {
-            if (activeCountdown != null) StopCoroutine(activeCountdown);
- 
-            examManager.ExitDangerZone();
+            StopCoroutine(activeCountdown);
+            activeCountdown = null;
         }
+
+        missileLauncher.DestroyActiveMissile();
+
+        examManager.ExitDangerZone();
     }
 
     private IEnumerator MissileCountdown()
     {
+        examManager.StartMissileCountdown();
+
         yield return new WaitForSeconds(missileDelay);
+
+        missileLauncher.Launch(playerTransform);
+        examManager.MissileLaunched();
+
+        activeCountdown = null;
     }
 }
